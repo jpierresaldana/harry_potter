34ede62802d8882cb9853d07ac2f41bbb138901a
Jean Pierre <jp.saldana@outlook.es>
2022-12-01 17:51:40 -0500

    Seed, controller, model ok

--- db/migrate/20221201221907_create_characters.rb
+++ db/migrate/20221201221907_create_characters.rb
@@ -2,7 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[7.0]
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :review
+      t.text :review
       t.string :actor
       t.string :image_url
 
